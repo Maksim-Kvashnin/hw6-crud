8159d91daff8ef5e5e25a62136c7b2c533305567
Maksim_KA <Kvashnin@hotmail.com>
2024-05-26 15:19:54 +0700

    done

---
+++ frontend/src/components/CRUD/Card/CRUDCard.tsx
@@ -0,0 +1,20 @@
+import { CrudCardProps } from "../CRUDTypes";
+import "./CRUDCard.css";
+
+type ExpandedCardType = CrudCardProps & { callback(id: number): void };
+
+export default function CRUDCard({ content, id, callback }: ExpandedCardType) {
+  return (
+    <div className="card">
+      <span
+        onClick={() => {
+          callback(id);
+        }}
+        className="card__close-btn"
+      >
+        &#10060;
+      </span>
+      <p className="card__content">{content}</p>
+    </div>
+  );
+}
